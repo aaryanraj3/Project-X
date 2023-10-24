08a48546ce8402b64c14e10442ae0a4c13727e3f
Aaryan Raj <69566713+aaryanraj3@users.noreply.github.com>
2023-10-24 23:36:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+//Color Pallete Selection on the basis of user's mode
+function detectColorScheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+// to detect changes in user's mode
+window.matchMedia('(prefers-color-scheme: dark)').addListener(detectColorScheme);
+
+// first detection of mode
+detectColorScheme();
+
+
+
+// CountDown Timer
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+// End date of the countdown
+const deadline = new Date(Date.parse(new Date("Dec 31,2023 00:00:00")));
+initializeClock('clockdiv', deadline);
+
+
+
